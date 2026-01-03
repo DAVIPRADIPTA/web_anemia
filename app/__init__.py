a2f7eecb1337bd8b370b47b0b612b3df3489ad91
DAVIPRADIPTA <davipradipta456@gmail.com>
2026-01-04 03:36:04 +0700

    uncoment model chatbot

--- app/__init__.py
+++ app/__init__.py
@@ -42,7 +42,7 @@ def create_app(config_class=Config):
     from app.routes.screening_routes import screening_bp
     from app.routes.consultation_routes import consultation_bp
     # (opsional) chatbot routes
-    # from app.routes.chatbot_routes import chatbot_bp
+    from app.routes.chatbot_routes import chatbot_bp
 
     from app.web.auth_routes import web_auth_bp
     from app.web.admin_routes import admin_bp
@@ -56,7 +56,7 @@ def create_app(config_class=Config):
     app.register_blueprint(article_bp)
     app.register_blueprint(screening_bp)
     app.register_blueprint(consultation_bp)
-    # app.register_blueprint(chatbot_bp)
+    app.register_blueprint(chatbot_bp)
 
     app.register_blueprint(web_auth_bp)
     app.register_blueprint(admin_bp)
